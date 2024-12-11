9aa4d2803d652a764306770131953cb81dbd9b48
Nirajjj11 <nirajkumar09160@gmail.com>
2024-12-11 13:49:41 +0530

    Second commit

---
+++ src/Components/DarkLightTheme.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react'
+
+function DarkLightTheme() {
+    const [dark, setDark]= useState("dark")
+    
+    return (
+        <div>DarkLightTheme</div>
+    )
+}
+
+export default DarkLightTheme
