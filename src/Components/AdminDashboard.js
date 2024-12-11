4376f060dfbaf8fa46c120c355b961b9b3036a64
Nirajjj11 <nirajkumar09160@gmail.com>
2024-12-11 13:44:24 +0530

    first commit

---
+++ src/Components/AdminDashboard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AdminDashboard() {
+    return (
+        <div>AdminDashboard
+            <h1>this is the admin pannel</h1>
+        </div>
+    )
+}
+
+export default AdminDashboard
---
+++ src/Components/Employees.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Employees() {
+  return (
+    <div>Employees
+        <h1>this is the employee details</h1>
+    </div>
+  )
+}
+
+export default Employees
